4465fe4adb69789efbe5bba08344dcd9c3020f3d
kempp-student <80429852+kempp-student@users.noreply.github.com>
2024-03-03 12:43:23 +1100

    feat: PK-1 initial version

---
+++ src/main/java/pk/demo/codepipeline/pkdemocodepipeline/PkDemoCodepipelineApplication.java
@@ -0,0 +1,13 @@
+package pk.demo.codepipeline.pkdemocodepipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkDemoCodepipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkDemoCodepipelineApplication.class, args);
+	}
+
+}
